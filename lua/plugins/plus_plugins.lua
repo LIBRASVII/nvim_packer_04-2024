1a53db5d7374db5d8bf518f009828396379a70d4
librasvii <moreiralucas511@gmail.com>
2024-04-14 20:14:17 -0300

    firts commit

---
+++ lua/plugins/plus_plugins.lua
@@ -0,0 +1,10 @@
+return {
+  -- WakaTime
+  { "wakatime/vim-wakatime", lazy = false },
+
+  -- JDTLS
+  { "mfussenegger/nvim-jdtls" },
+
+  -- Terminal to nvim
+  { "akinsho/toggleterm.nvim", version = "*", config = true },
+}
